e5eb169f5a7ef8fecc1ecab682402287cbc484fd
IDAN2468D <48420836+IDAN2468D@users.noreply.github.com>
2021-06-22 17:18:31 +0300

    Update

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import {ActivityIndicator} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Tabs from "./navigation/tabs";
+import * as Font from 'expo-font';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+    const customFonts = {
+        "Roboto-Black": require('./assets/fonts/Roboto-Black.ttf'),
+        "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+        "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf")
+      };
+      
+      const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+      const _loadAssetsAsync = async () => {
+        await Font.loadAsync(customFonts);
+        setAssetsLoaded(true);
+      }
+    
+      useEffect(() => {
+        _loadAssetsAsync();
+      });
+
+    return assetsLoaded ? (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'MainLayout'}
+            >
+                <Stack.Screen
+                    name="MainLayout"
+                    component={Tabs}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    ): (
+        <ActivityIndicator size="small" />
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/MainLayout..js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    View
+}
+    from 'react-native';
+import { COLORS, SIZES, icons } from '../components';
+
+function MainLayout() {
+    return (
+        <View
+        
+        >
+
+        </View>
+    )
+}
+
+export default MainLayout
